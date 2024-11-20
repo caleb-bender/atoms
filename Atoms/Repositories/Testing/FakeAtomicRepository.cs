521ab8395088ee76edd88351a579527477d0c16a
Caleb Bender <caleb.c.bender@gmail.com>
2024-11-19 23:56:24 -0800

    Implement FakeAtomicRepository for unit testing repository code

---
+++ Atoms/Repositories/Testing/FakeAtomicRepository.cs
@@ -0,0 +1,111 @@
+﻿using CalebBender.Atoms.DataAttributes;
+using CalebBender.Atoms.Exceptions;
+using CalebBender.Atoms.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalebBender.Atoms.Repositories.Testing
+{
+    public class FakeAtomicRepository<TModel> : IAtomicRepository<TModel> where TModel : class, new()
+    {
+        private readonly List<TModel> testCollection;
+
+        public FakeAtomicRepository(List<TModel> testCollection)
+        {
+            if (testCollection is null)
+                throw new ArgumentNullException("The test collection passed to the FakeAtomicRepository constructor cannot be null");
+            this.testCollection = testCollection;
+        }
+
+        public Task<IEnumerable<TModel>> CreateManyAsync(IEnumerable<TModel> models)
+        {
+            if (models is null) return Task.FromResult(new List<TModel>() as IEnumerable<TModel>);
+            foreach (var model in models)
+            {
+                if (model is null)
+                    throw new ArgumentNullException("A model passed to CreateManyAsync cannot be null");
+                if (GetSavedModel(model) is not null)
+                    throw new DuplicateUniqueIdException("The model being created already exists in the test collection");
+                testCollection.Add(model);
+            }
+            return Task.FromResult(models);
+        }
+
+        public Task<int> DeleteManyAsync(IEnumerable<TModel> models)
+        {
+            if (models is null) return Task.FromResult(0);
+            int numberDeleted = 0;
+            foreach (var model in models)
+            {
+                if (model is null)
+                    throw new ArgumentNullException("A model passed to DeleteManyAsync cannot be null");
+                var removed = testCollection.Remove(model);
+                if (removed) numberDeleted++;
+            }
+            return Task.FromResult(numberDeleted);
+        }
+
+        public Task<AtomicOption<TModel>> GetOneAsync(TModel model)
+        {
+            if (model is null)
+                throw new ArgumentNullException("The model passed to GetOneAsync cannot be null");
+            if (!testCollection.Contains(model))
+                return Task.FromResult(new AtomicOption<TModel>.Empty() as AtomicOption<TModel>);
+            return Task.FromResult(new AtomicOption<TModel>.Exists(model) as AtomicOption<TModel>);
+        }
+
+        public Task<int> UpdateManyAsync(IEnumerable<TModel> models)
+        {
+            if (models is null) return Task.FromResult(0);
+            int numberUpdated = 0;
+            foreach (var model in models)
+            {
+                if (model is null)
+                    throw new ArgumentNullException("A model passed to UpdateManyAsync cannot be null");
+                var savedModel = GetSavedModel(model);
+                if (savedModel is not null)
+                {
+                    UpdateSavedModelPropertiesInPlace(savedModel, model);
+                    numberUpdated++;
+                }
+            }
+            return Task.FromResult(numberUpdated);
+        }
+
+        private TModel? GetSavedModel(TModel model)
+        {
+            return testCollection.Find(m => UniqueIdsMatch(m, model));
+        }
+
+        private void UpdateSavedModelPropertiesInPlace(TModel savedModel, TModel model)
+        {
+            var properties = typeof(TModel).GetProperties();
+            foreach (var property in properties)
+            {
+                if (Attribute.IsDefined(property, typeof(UniqueIdAttribute)))
+                    continue;
+                var value = property.GetValue(model);
+                property.SetValue(savedModel, value);
+            }
+        }
+
+        private bool UniqueIdsMatch(TModel model1, TModel model2)
+        {
+            var properties = typeof(TModel).GetProperties();
+            foreach (var property in properties)
+            {
+                if (Attribute.IsDefined(property, typeof(UniqueIdAttribute)))
+                {
+                    var value1 = property.GetValue(model1);
+                    var value2 = property.GetValue(model2);
+                    if (!value1!.Equals(value2))
+                        return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ AtomsUnitTests/RepositoriesTests/FakeAtomicRepositoryTests.cs
@@ -0,0 +1,323 @@
+﻿using CalebBender.Atoms.DataAttributes;
+using CalebBender.Atoms.Exceptions;
+using CalebBender.Atoms.Repositories;
+using CalebBender.Atoms.Repositories.Testing;
+using CalebBender.Atoms.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomsUnitTests.RepositoriesTests
+{
+    public class FakeAtomicRepositoryTests
+    {
+        class PlayerStats
+        {
+            [UniqueId]
+            public int Id { get; set; }
+            [UniqueId]
+            public int PlayerId { get; set; }
+            public int Health { get; set; }
+            public int Armor { get; set; }
+            public int Damage { get; set; }
+        }
+
+
+        List<PlayerStats> playerStatsCollection = new();
+        IAtomicRepository<PlayerStats> fakeRepo;
+        PlayerStats stats = new PlayerStats { Id = 1, PlayerId = 1, Health = 100, Armor = 10, Damage = 20 };
+        PlayerStats stats2 = new PlayerStats { Id = 2, PlayerId = 1, Health = 90, Armor = 5, Damage = 15 };
+        PlayerStats stats3 = new PlayerStats { Id = 3, PlayerId = 2, Health = 80, Armor = 0, Damage = 10 };
+
+        PlayerStats statsUpdated = new PlayerStats { Id = 1, PlayerId = 1, Health = 50, Armor = 5, Damage = 10 };
+        PlayerStats stats2Updated = new PlayerStats { Id = 2, PlayerId = 1, Health = 40, Armor = 4, Damage = 8 };
+        PlayerStats stats3Updated = new PlayerStats { Id = 3, PlayerId = 2, Health = 30, Armor = 3, Damage = 6 };
+
+        public FakeAtomicRepositoryTests()
+        {
+            fakeRepo = new FakeAtomicRepository<PlayerStats>(playerStatsCollection);
+        }
+
+        [Fact]
+        public void GivenANullCollection_WhenInitializingFakeRepo_ThenAnArgumentNullExceptionIsThrown()
+        {
+            // Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                // Act
+                var fakeRepo = new FakeAtomicRepository<PlayerStats>(null);
+            });
+        }
+
+        [Fact]
+        public async Task GivenAnEmptyCollection_WhenEntryIsCreated_ThenCollectionContainsEntry()
+        {
+            // Act
+            await fakeRepo.CreateOneAsync(stats);
+            // Assert
+            Assert.Single(playerStatsCollection);
+            Assert.Contains(stats, playerStatsCollection);
+        }
+
+        [Fact]
+        public async Task GivenAnEmptyCollection_WhenEntryIsCreatedAndRetrieved_ItIsAtomicOptionExists()
+        {
+            // Act
+            await fakeRepo.CreateOneAsync(stats);
+            var retrievedStatsResult = await fakeRepo.GetOneAsync(stats);
+            // Assert
+            var exists = Assert.IsType<AtomicOption<PlayerStats>.Exists>(retrievedStatsResult);
+            Assert.Equal(stats, exists.Value);
+        }
+
+        [Fact]
+        public async Task GivenAnEmptyCollection_WhenNonexistentEntryIsRetrieved_ThenItIsAtomicOptionEmpty()
+        {
+            // Act
+            var result = await fakeRepo.GetOneAsync(stats);
+            // Assert
+            Assert.IsType<AtomicOption<PlayerStats>.Empty>(result);
+        }
+
+        [Fact]
+        public async Task WhenEntryIsCreatedAndDeleted_ThenCollectionIsEmpty()
+        {
+            // Act
+            await fakeRepo.CreateOneAsync(stats);
+            await fakeRepo.DeleteOneAsync(stats);
+            // Assert
+            Assert.Empty(playerStatsCollection);
+        }
+
+        [Fact]
+        public async Task WhenEntryIsCreatedAndDeleted_ThenOneIsReturnedFromDelete()
+        {
+            // Act
+            await fakeRepo.CreateOneAsync(stats);
+            var result = await fakeRepo.DeleteOneAsync(stats);
+            // Assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public async Task GivenAnEmptyCollection_WhenEntryIsDeleted_ThenZeroIsReturnedFromDelete()
+        {
+            // Act
+            var result = await fakeRepo.DeleteOneAsync(stats);
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public async Task GivenAnEmptyCollection_WhenEntriesAreCreated_ThenTheCreatedEntriesAreReturned()
+        {
+            // Act
+            var created = await fakeRepo.CreateManyAsync(new List<PlayerStats> { stats, stats2, stats3 });
+            // Assert
+            Assert.Equal(3, playerStatsCollection.Count);
+            Assert.Equal(3, created.Count());
+            Assert.Contains(stats, playerStatsCollection);
+            Assert.Contains(stats2, playerStatsCollection);
+            Assert.Contains(stats3, playerStatsCollection);
+        }
+
+        [Fact]
+        public async Task GivenAnEmptyCollection_WhenAnNonexistentEntryIsUpdated_ThenZeroIsReturned()
+        {
+            // Act
+            var result = await fakeRepo.UpdateOneAsync(stats);
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public async Task GivenThreeCreatedEntries_WhenTwoAreUpdated_ThenTwoIsReturned()
+        {
+            // Arrange
+            playerStatsCollection.AddRange(new List<PlayerStats> { stats, stats2, stats3 });
+            // Act
+            var result = await fakeRepo.UpdateManyAsync(new List<PlayerStats> { stats, stats2 });
+            // Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public async Task GivenSomeCreatedEntries_WhenTheEntryIsUpdatedUsingADeepCopyWithSameIds_ThenTheEntryInTheTestCollectionIsUpdated()
+        {
+            // Arrange
+            playerStatsCollection.Add(stats);
+            playerStatsCollection.Add(stats2);
+            playerStatsCollection.Add(stats3);
+            var updatedStats = new PlayerStats { Id = 1, PlayerId = 1, Health = 50, Armor = 5, Damage = 10 };
+            // Act
+            await fakeRepo.UpdateOneAsync(updatedStats);
+            // Assert
+            Assert.Equal(3, playerStatsCollection.Count);
+            Assert.Contains(stats, playerStatsCollection);
+            Assert.Equal(50, stats.Health);
+            Assert.Equal(5, stats.Armor);
+            Assert.Equal(10, stats.Damage);
+        }
+
+        [Fact]
+        public async Task GivenThreeCreatedEntries_WhenAllAreUpdated_ThenThreeIsReturned_And_TheEntriesInTheTestCollectionAreUpdated()
+        {
+            // Arrange
+            playerStatsCollection.AddRange(new List<PlayerStats> { stats, stats2, stats3 });
+            var newStats = new List<PlayerStats>
+            {
+                statsUpdated, stats2Updated, stats3Updated
+            };
+            // Act
+            var result = await fakeRepo.UpdateManyAsync(newStats);
+            // Assert
+            Assert.Equal(3, result);
+            Assert.Equal(3, playerStatsCollection.Count);
+            Assert.Contains(stats, playerStatsCollection);
+            Assert.Contains(stats2, playerStatsCollection);
+            Assert.Contains(stats3, playerStatsCollection);
+            Assert.Equal(50, stats.Health);
+            Assert.Equal(5, stats.Armor);
+            Assert.Equal(10, stats.Damage);
+            Assert.Equal(40, stats2.Health);
+            Assert.Equal(4, stats2.Armor);
+            Assert.Equal(8, stats2.Damage);
+            Assert.Equal(30, stats3.Health);
+            Assert.Equal(3, stats3.Armor);
+            Assert.Equal(6, stats3.Damage);
+        }
+
+        [Fact]
+        public async Task WhenTwoEntriesWithMatchingUniqueIdsAreCreated_ThenDuplicateUniqueIdExceptionIsThrown()
+        {
+            // Assert
+            await Assert.ThrowsAsync<DuplicateUniqueIdException>(() =>
+            {
+                // Act
+                return fakeRepo.CreateManyAsync(stats, statsUpdated);
+            });
+        }
+
+        [Fact]
+        public async Task GivenThreeCreatedEntries_WhenTwoAreDeleted_ThenTwoIsReturned_And_TheTestCollectionContainsOneEntry()
+        {
+            // Arrange
+            await fakeRepo.CreateManyAsync(new List<PlayerStats> { stats, stats2, stats3 });
+            // Act
+            var result = await fakeRepo.DeleteManyAsync(new List<PlayerStats> { stats, stats2 });
+            // Assert
+            Assert.Equal(2, result);
+            Assert.Single(playerStatsCollection);
+            Assert.Contains(stats3, playerStatsCollection);
+        }
+
+        [Fact]
+        public async Task WhenNullCollectionOfEntriesIsPassedToCreate_ThenNothingIsCreated()
+        {
+            // Act
+            var created = await fakeRepo.CreateManyAsync(null);
+            // Assert
+            Assert.Empty(playerStatsCollection);
+            Assert.NotNull(created);
+            Assert.Empty(created);
+        }
+
+        [Fact]
+        public async Task WhenEmptyCollectionOfEntriesIsPassedToCreate_ThenNothingIsCreated()
+        {
+            // Act
+            var created = await fakeRepo.CreateManyAsync(new List<PlayerStats>());
+            // Assert
+            Assert.Empty(playerStatsCollection);
+            Assert.NotNull(created);
+            Assert.Empty(created);
+        }
+
+
+        [Fact]
+        public async Task WhenNullCollectionOfEntriesIsPassedToUpdate_ThenNothingIsUpdated()
+        {
+            // Act
+            var updated = await fakeRepo.UpdateManyAsync(null);
+            // Assert
+            Assert.Empty(playerStatsCollection);
+            Assert.Equal(0, updated);
+        }
+
+        [Fact]
+        public async Task WhenEmptyCollectionOfEntriesIsPassedToUpdate_ThenNothingIsUpdated()
+        {
+            // Act
+            var updated = await fakeRepo.UpdateManyAsync(new List<PlayerStats>());
+            // Assert
+            Assert.Empty(playerStatsCollection);
+            Assert.Equal(0, updated);
+        }
+
+        [Fact]
+        public async Task WhenCreatingNullEntries_AnArgumentNullExceptionIsThrown()
+        {
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() =>
+            {
+                // Act
+                return fakeRepo.CreateManyAsync(new List<PlayerStats> { null, null });
+            });
+        }
+
+        [Fact]
+        public async Task WhenUpdatingUsingNullEntries_AnArgumentNullExceptionIsThrown()
+        {
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() =>
+            {
+                // Act
+                return fakeRepo.UpdateManyAsync(new List<PlayerStats> { null, null });
+            });
+        }
+
+        [Fact]
+        public async Task WhenNullCollectionOfEntriesIsPassedToDelete_ThenNothingIsDeleted()
+        {
+            // Act
+            var deleted = await fakeRepo.DeleteManyAsync(null);
+            // Assert
+            Assert.Empty(playerStatsCollection);
+            Assert.Equal(0, deleted);
+        }
+
+        [Fact]
+        public async Task WhenEmptyCollectionOfEntriesIsPassedToDelete_ThenNothingIsDeleted()
+        {
+            // Act
+            var deleted = await fakeRepo.DeleteManyAsync(new List<PlayerStats>());
+            // Assert
+            Assert.Empty(playerStatsCollection);
+            Assert.Equal(0, deleted);
+        }
+
+        [Fact]
+        public async Task WhenDeletingNullEntries_AnArgumentNullExceptionIsThrown()
+        {
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() =>
+            {
+                // Act
+                return fakeRepo.DeleteManyAsync(new List<PlayerStats> { null, null });
+            });
+        }
+
+        [Fact]
+        public async Task WhenGettingAnEntryUsingNull_ThenAnArgumentNullExceptionIsThrown()
+        {
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() =>
+            {
+                // Act
+                return fakeRepo.GetOneAsync(null);
+            });
+        }
+    }
+}
